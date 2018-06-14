959f0fc986b89ef0cd41cd262d8c14d41987a544
mjonatanm <30849135+mjonatanm@users.noreply.github.com>
2018-06-14 18:12:03 -0300

    Mod_en_Proyecto
    Modificando el proyecto .net

--- HolaMundo/HolaMundo/Program.cs
+++ HolaMundo/HolaMundo/Program.cs
@@ -10,7 +10,7 @@ namespace HolaMundo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola Mundo!");
+            Console.WriteLine("Hola Mundo desde el local!");
             Console.ReadLine();
         }
     }
